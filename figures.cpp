f06657f4ae0616c7b5a7fac8ce1f50bc4fc78782
tima55510 <tima55510@gmail.com>
2015-11-29 18:09:44 +0300

    7.2

--- figures.cpp
+++ figures.cpp
@@ -11,22 +11,27 @@ void createPieces(pieces * pieces) {
 	pieces->firstPieces = new RectangleShape(Vector2f(STANDARD_SIZE_PIECES));
 	pieces->firstPieces->setPosition(Vector2f(INITIAL_COORD_FIRTS_PIECES));
 	pieces->firstPieces->setFillColor(COLOR_PIECES);
+	pieces->firstPieces->setOrigin(STANDARD_SIZE_PIECES.x / 2.0f, STANDARD_SIZE_PIECES.y / 2.0f);
 
 	pieces->secondPieces = new RectangleShape(Vector2f(STANDARD_SIZE_PIECES));
 	pieces->secondPieces->setPosition(Vector2f(INITIAL_COORD_SECOND_PIECES));
 	pieces->secondPieces->setFillColor(COLOR_PIECES);
+	pieces->secondPieces->setOrigin(STANDARD_SIZE_PIECES.x / 2.0f, STANDARD_SIZE_PIECES.y / 2.0f);
 
 	pieces->thirdPieces = new RectangleShape(Vector2f(STANDARD_SIZE_PIECES));
 	pieces->thirdPieces->setPosition(Vector2f(INITIAL_COORD_THIRD_PIECES));
 	pieces->thirdPieces->setFillColor(COLOR_PIECES);
+	pieces->thirdPieces->setOrigin(STANDARD_SIZE_PIECES.x / 2.0f, STANDARD_SIZE_PIECES.y / 2.0f);
 
 	pieces->fourthPieces = new RectangleShape(Vector2f(STANDARD_SIZE_PIECES));
 	pieces->fourthPieces->setPosition(Vector2f(INITIAL_COORD_FOURTH_PIECES));
 	pieces->fourthPieces->setFillColor(COLOR_PIECES);
+	pieces->fourthPieces->setOrigin(STANDARD_SIZE_PIECES.x / 2.0f, STANDARD_SIZE_PIECES.y / 2.0f);
 
 	pieces->fifthPieces = new RectangleShape(Vector2f(STANDARD_SIZE_PIECES));
 	pieces->fifthPieces->setPosition(Vector2f(INITIAL_COORD_FIFTH_PIECES));
 	pieces->fifthPieces->setFillColor(COLOR_PIECES);
+	pieces->fifthPieces->setOrigin(STANDARD_SIZE_PIECES.x / 2.0f, STANDARD_SIZE_PIECES.y / 2.0f);
 
 }
 
@@ -56,9 +61,19 @@ void secondStep(pieces * figures) {
 		figures->thirdPieces->setFillColor(Color::Color(21, 21, 247, 255 * figures->capacity));
 		figures->fourthPieces->setFillColor(Color::Color(21, 21, 247, 255 * figures->capacity));
 		figures->fifthPieces->setFillColor(Color::Color(21, 21, 247, 255 * figures->capacity));
+		figures->thirdPieces->rotate(0.5f);
+		figures->firstPieces->rotate(0.5f);
+		figures->secondPieces->rotate(-0.5f);
+		figures->fifthPieces->rotate(0.5f);
+		figures->fourthPieces->rotate(-0.5f);
 		figures->capacity -= 0.00003f;
 	}
 	else {
+		figures->firstPieces->setRotation(0.0f);
+		figures->secondPieces->setRotation(0.0f);
+		figures->thirdPieces->setRotation(0.0f);
+		figures->fourthPieces->setRotation(0.0f);
+		figures->fifthPieces->setRotation(0.0f);
 		figures->step = 3;
 	}
 }
@@ -98,8 +113,6 @@ void fourthStep(pieces * figures) {
 		figures->thirdPieces->setScale(figures->firstPieces->getScale().x + figures->reductionFactor, figures->firstPieces->getScale().y + figures->reductionFactor);
 		figures->fourthPieces->setScale(figures->firstPieces->getScale().x + figures->reductionFactor, figures->firstPieces->getScale().y + figures->reductionFactor);
 		figures->fifthPieces->setScale(figures->firstPieces->getScale().x + figures->reductionFactor, figures->firstPieces->getScale().y + figures->reductionFactor);
-
-
 		figures->capacity += 0.00005f;
 	}
 	else {
